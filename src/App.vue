e533bb9fa432b58247b6a079b06cb0ba6954d82b
Bia Garrido <36662037+bgarrido7@users.noreply.github.com>
2023-04-19 17:30:04 +0100

    navbar done

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <HelloWorld msg="my-navbar"/>
+  <MyPage msg="my-page"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyPage from './components/MyPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyPage
+  },
+}
+</script>
+
+<style>
+
+html{
+  height: 100%;
+}
+
+body{
+  margin:0;
+  height: 100%;
+  font-family: 'Courier New', Courier, monospace;  
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app{
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+<nav>
+  <ul>
+    <li><a href="#Oscars">Oscars 🏆</a></li>
+    <li><a href="#Movies">Movies</a></li>
+    <li><a href="#Shows">Tv Shows</a></li>
+    <li><a href="#Anime">Anime</a></li>
+    <li><a href="#Games">Video Games</a></li>
+    <li><a href="#Board">Board Games</a></li>
+  </ul>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+nav {
+  background-color:#2c3e50;
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+
+  height: 100%;
+  overflow: hidden;
+  width: 20vw;
+
+}
+ul {
+  width: 100%;
+  padding: 0;
+  list-style: none;
+}
+
+li{
+  padding: 3vh 0;
+  width: 100%;
+  margin: 3vh 0;
+}
+li:hover{
+  background-color:#1f2c38;
+  cursor: pointer;
+}
+a {
+  color: white;
+  text-decoration: none;
+}
+
+</style>

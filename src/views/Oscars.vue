2d2e76cf860c11d8b106f69a4b0cbbadaed9635f
bgarrido7 <bianevesgarrido@gmail.com>
2023-05-03 17:17:09 +0100

    add data

--- src/views/Oscars.vue
+++ src/views/Oscars.vue
@@ -1,26 +1,60 @@
+<script>
+import myJson from '../data/oscars.json'
+
+export default {
+  name: 'OscarsPredic',
+
+  data() {
+    return {
+      oscarYear: "2023",
+      awards: myJson,
+    }
+  },
+  methods: {
+    countWins: function (awards, oscarYear) {
+      let count = 0;
+
+      Object.entries(awards[oscarYear]).forEach(award => {
+        if (award[1].bet.toLowerCase() === award[1].winner.toLowerCase())
+          count++;
+
+      })
+      return count + '/ ' + Object.keys(awards[oscarYear]).length;
+    },
+
+  }
+
+
+
+};
+</script>
+
 <template >
   <div class="oscars-predictions">
 
+    <div class="header">
     <div class="year">
       <label for="oscarYear">Select the Year</label>
-      <select name="year" id="oscarYear">
-        <option selected value="2023">2023</option>
-        <option value="2023">2022</option>
-        <option value="2023">2021</option>
-        <option value="2023">2020</option>
-        <option value="2023">2019</option>
+      <select name="year" id="oscarYear" v-model="oscarYear">
+        <option value="2023">2023</option>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+        <option value="2020">2020</option>
+        <option value="2019">2019</option>
       </select>
     </div>
 
-    <div class="my-wins"> 10/20</div>
+    <div class="my-wins">Correct Bets: {{ countWins(awards, oscarYear) }} </div>
+
+  </div>
 
     <div class="awards">
-      <div v-for="award in awards" :key="award.category">
+      <div v-for="award in awards[oscarYear]" :key="award">
         <h1>{{ award.category }}</h1>
         <ul>
           <li v-for="(movie, index) in award.movies" v-bind:key="index"
-            v-bind:class="movie === award.winner ? 'winner' : ''">
-            {{ movie }} <span class="bet" v-if="award.bet === movie"> &lt;-- </span>
+            :class="movie.toLowerCase() === award.winner.toLowerCase() ? 'winner' : ''">
+            {{ movie }} <span class="bet" v-if="award.bet === movie"> ↙ </span>
           </li>
         </ul>
       </div>
@@ -29,41 +63,20 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'OscarsPredic',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      awards: [{
-        category: 'Best Picture',
-        movies: ['Everything, Everywhere, All at Once', 'Tár', 'All Quiet in the Western front'],
-        winner: 'Everything, Everywhere, All at Once',
-        bet: 'Everything, Everywhere, All at Once'
-      },
-      {
-        category: 'Directing',
-        movies: ['Everything, Everywhere, All at Once', 'Triangle of sadness', 'Tár', 'The Fabelmans'],
-        winner: 'Everything, Everywhere, All at Once',
-        bet: 'Tár'
-      }
-      ]
-    }
-  }
-};
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style scoped>
 .oscars-predictions {
-  display: grid;
-  /* columns: 1fr 1fr; */
-  padding: 2vw;
+  display: flex;
+  flex-direction: column;
+  padding: 3vmin;
+  width: 100%;
 }
 
+.header{
+  display: flex;
+  justify-content: space-between;
+}
 
 .my-wins {
   align-self: auto;
@@ -77,13 +90,28 @@ export default {
 }
 
 .awards {
+  padding: 2vmin;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5vw;
   text-transform: capitalize;
 }
 
+ul{
+  list-style: none;
+  padding:0;
+}
+
+span{
+  position: relative;
+  bottom: 7px;
+  right: 3px;
+  color: green;
+  font-weight: 900;
+  font-size: 3vmin;
+}
 .winner {
   color: goldenrod
 }
 
-.bet {
-  color: greenyellow
-}</style>
+</style>

f899385be00ad44450da280a63fc8d453f2e43bd
Bia Garrido <36662037+bgarrido7@users.noreply.github.com>
2025-04-20 11:34:28 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'my-likes',
   plugins: [
     vue(),
     Components({

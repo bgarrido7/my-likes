dbc8e9e7c5798fb757d6ff3fa69788501ae059a5
Beatriz Garrido <beatriz.garrido@ctw.bmwgroup.com>
2025-04-21 14:24:28 +0100

    books page

--- src/components/views/Books.vue
+++ src/components/views/Books.vue
@@ -1,31 +1,92 @@
 <template>
   <div class="books-page">
-    <h2>books</h2>
-    <div>
-      <n-button-group horizontal>
+    <div class="filter">
+      <n-button-group>
         <n-button> Novels </n-button>
+        <n-button> Psychology / Non-fiction </n-button>
+        <n-button> Comics & Graphic Novel </n-button>
         <n-button> Manga </n-button>
-        <n-button> Comic </n-button>
       </n-button-group>
     </div>
-
-    <EmptyState />
+    <div class="content">
+      <n-card v-for="(book, key) in content" :key="key" :title="book.name">
+        <template #cover>
+          <img :src="book.cover" />
+        </template>
+        <div class="description">
+          <span>{{ book.author }}</span>
+          <div class="footer">
+            <span>{{ book.year }}</span>
+            <a :href="book.url" target="_blank">Goodreads</a>
+          </div>
+        </div>
+      </n-card>
+    </div>
   </div>
 </template>
 
 <script setup>
-import EmptyState from "@/components/shared/EmptyState.vue";
+import { ref, onMounted } from "vue";
 import { NButton } from "naive-ui";
 import { NButtonGroup } from "naive-ui";
+import { NCard } from "naive-ui";
+
+const jsonUrl = `${import.meta.env.BASE_URL}data/books.json`;
+const content = ref([]);
+
+onMounted(async () => {
+  try {
+    const res = await fetch(jsonUrl);
+    if (!res.ok) throw new Error("Failed to load data");
+    const jsonData = await res.json();
+    content.value = jsonData.content;
+  } catch (error) {
+    console.error("Error loading JSON data:", error);
+  }
+});
 </script>
 
 <style scoped>
 .books-page {
   padding: 1vw;
+  padding-bottom: 0;
+  overflow-y: auto;
+  max-height: 100%;
+}
+
+.content {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 1fr);
+  grid-auto-flow: column;
+  justify-content: start;
+  gap: 1vw;
+  padding: 2vw;
+}
+
+@media (max-width: 990px) {
+  .content {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .n-card {
+    width: 150px;
+  }
+}
+
+.n-card {
+  max-width: 250px;
+}
+
+.description {
   display: flex;
   flex-direction: column;
+  height: 100%;
 }
-h2 {
-  text-transform: capitalize;
+
+.footer {
+  display: flex;
+  align-items: end;
+  justify-content: space-between;
+  flex-grow: 1;
 }
 </style>
